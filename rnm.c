20a2efd3da3d58470fca94a23a9862ff7c751f89
ProX <81921840+Pr00x@users.noreply.github.com>
2021-11-06 21:38:34 +0100

    Add files via upload

--- rnm.c
+++ rnm.c
@@ -2,21 +2,21 @@
 #include<string.h>
 
 int main(int argc, char *argv[]) {
-	if(!argv[1]) {
+	if(argv[1] == NULL) {
 		puts("rnm: missing operand.\x0ATry 'rnm --help' for more information.");
 
 		return -1;
 	}
-	else if(strncmp(argv[1], "--help", 7) == 0 || strncmp(argv[1], "-h", 3) == 0 && !argv[2]) {
+	else if(strncmp(argv[1], "--help", 7) == 0 || strncmp(argv[1], "-h", 3) == 0) {
 		puts("Usage: rnm [FILE] [NEW FILENAME]\x0ARename the FILE.");
 	
 		return 0;
 	}
-	else if(!argv[1] || !argv[2]) {
-    		puts("rnm: missing operand.\x0ATry 'rnm --help' for more information.");
+	else if(argv[2] == NULL) {
+		puts("rnm: missing operand.\x0ATry 'rnm --help' for more information.");
 
-    		return -1;
-  	}
+		return -1;
+	}
 
 	int rnm = rename(argv[1], argv[2]);
 
